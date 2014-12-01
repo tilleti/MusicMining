469b37a383ae304c4729e83ef85b74d3fd05b90f
Colyette <colyette3@gmail.com>
2014-11-30 20:49:31 -0500

    adding cluster separation to the kmeans script

--- musickmeans.r
+++ musickmeans.r
@@ -1,4 +1,7 @@
 # Performs kmeans on numerical attributes of music dataset, k=10
+# When R is ran in the same directory as the single attrib data file
+# named here as singleAtt.csv, created from p3_h5tocsv.py
+# > source("musickmeans.r") 
 musicDS <- new.env()
 
 library(rattle)
@@ -24,4 +27,20 @@ evalq({
 	}, musicKMEANS)
 
 
+######separate clusters
+#get cluster vector (seems to only do a subset of data...3742 rows)
+musicClusters <- musicKMEANS$model$cluster
+
+#separate rows into their cluster
+c1 <- music[musicClusters == 1,]
+c2 <- music[musicClusters == 2,]
+c3 <- music[musicClusters == 3,]
+c4 <- music[musicClusters == 4,]
+c5 <- music[musicClusters == 5,]
+c6 <- music[musicClusters == 6,]
+c7 <- music[musicClusters == 7,]
+c8 <- music[musicClusters == 8,]
+c9 <- music[musicClusters == 9,]
+c10 <- music[musicClusters == 10,]
+#clusters seem to add up to 10,000 regardless...
 
