d403790485b8c194cfa0c32549ebe0703c0dfa5b
Colyette <colyette3@gmail.com>
2014-12-09 04:39:53 -0500

    adding R readme for clustering

--- musickmeans.r
+++ musickmeans.r
@@ -2,6 +2,10 @@
 # When R is ran in the same directory as the single attrib data file
 # named here as singleAtt.csv, created from p3_h5tocsv.py
 # > source("musickmeans.r") 
+
+#set seed 
+set.seed(42)
+
 musicDS <- new.env()
 
 library(rattle)
@@ -36,7 +40,17 @@ musicKMEANS <- new.env(parent=musicDS)
 #removed long & lat for sparse data is given
 #####vars <- c(vars[1:2],vars[5:6],vars[19:21],vars[24:25],vars[27:31],vars[32:35],vars[52])
 ##GOODish vars <- c(vars[1:2],vars[5:6],vars[19:21],vars[24:25],vars[27:31],vars[32:35],vars[52])
-vars <- c(vars[1:2],vars[14],vars[21],vars[23:35],vars[37:49],vars[52])
+
+#all num, minus ident, minus long & lat
+#vars <- c(vars[1:2],vars[14],vars[21],vars[23:35],vars[37:49],vars[52])
+
+#all num, minus ident, minus long & lat, minus confidence and weights
+vars <- c(vars[1:2],vars[14],vars[21],vars[23:27],vars[29:30],vars[32:34],vars[37],vars[39:42],vars[44],vars[46],vars[48],vars[52])
+
+#most correlated
+#vars <- c(vars[1:2],vars[24],vars[32],vars[39:40],vars[43],vars[48])
+
+
 music1 <- music[,vars]
 
 musicKMEANS <- new.env(parent=musicDS)
@@ -76,6 +90,9 @@ c1_2 <- c1[musicClusters1 == 2,]
 c1_3 <- c1[musicClusters1 == 3,]
 c1_4 <- c1[musicClusters1 == 4,]
 
+#write out data
+#write.csv(c1_1,"clust1_1.csv")
+
 ##split again c2
 c2k <- kmeans(x=na.omit(c2[,vars]),centers=5)
 musicClusters2 <-c2k$cluster
@@ -88,7 +105,7 @@ c2_4 <- c2[musicClusters2 == 4,]
 c2_5 <- c2[musicClusters2 == 5,]
 
 ##split again c3
-c3k <- kmeans(x=na.omit(c3[,vars]),centers=4)
+c3k <- kmeans(x=na.omit(c3[,vars]),centers=9)
 musicClusters3 <-c3k$cluster
 
 #split c3 data
@@ -120,3 +137,15 @@ c5_3 <- c5[musicClusters5 == 3,]
 c5_4 <- c5[musicClusters5 == 4,]
 c5_5 <- c5[musicClusters5 == 5,]
 
+#~~~~~~~~~~~~~~~~~~~~3rd partition pass~~~~~~~~
+#all from c1
+#c1_1
+#c1_1k <- kmeans(x=na.omit(c1_1[,vars]),centers=5)
+#musicClusters1_1 <-c1_1k$cluster
+
+#c1_1_1 <- c1_1[musicClusters1_1 == 1,]
+#c1_1_2 <- c1_1[musicClusters1_1 == 2,]
+#c1_1_3 <- c1_1[musicClusters1_1 == 3,]
+#c1_1_4 <- c1_1[musicClusters1_1 == 4,]
+#c1_1_5 <- c1_1[musicClusters1_1 == 5,]
+#c1_2
